e9b8db81eb76ac0adebed0c46c015c6fc4962c5c
UMARUZBEK <umarozbe@gmail.com>
2025-03-14 17:06:44 +0900

    fix: integrate global pipe validation

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,6 @@
 import { Mutation,Resolver,Query, Args } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException, UsePipes, ValidationPipe } from '@nestjs/common';
+import { InternalServerErrorException } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member';
 
@@ -10,7 +10,7 @@ export class MemberResolver {
     constructor(private readonly memberService: MemberService) {}
 
         @Mutation(() => Member)//post
-        @UsePipes(ValidationPipe)
+        
         public async signup(@Args("input")input:MemberInput ): Promise<Member> {
             try {  
                 console.log(" Mutation: signup ");
@@ -23,7 +23,7 @@ export class MemberResolver {
         }
 
         @Mutation(() => Member)
-        @UsePipes(ValidationPipe)
+
         public async login(@Args('input')input: LoginInput): Promise<Member>{
             try {            
             console.log("Mutation: Login");
@@ -33,6 +33,7 @@ export class MemberResolver {
                 throw new InternalServerErrorException(err);
             } 
            }
+
             @Mutation(() => String)
             public async updateMember(): Promise<string> {
                 console.log(" Mutation: updateMember ");
--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
  // express va nest jsni umumiy birlashmasi 
  
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
