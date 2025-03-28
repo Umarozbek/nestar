f2da207fe2628a2989f224389a826c568a466596
UMARUZBEK <umarozbe@gmail.com>
2025-03-28 21:25:11 +0900

    feat: develop GetAgentProperties GraphQL API'S

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { PropertyService } from './property.service';
-import { PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
+import {  AgentPropertiesInquiry, PropertiesInquiry, PropertyInput } from '../../libs/dto/property/property.input';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/types/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
@@ -68,4 +68,14 @@ export class PropertyResolver {
       return await this.propertyService.getProperties(memberId, input);
   }
 
+ @Roles(MemberType.AGENT)
+    @UseGuards(RolesGuard)
+    @Query((returns) => Properties)
+    public async getAgentProperties(
+        @Args('input') input: AgentPropertiesInquiry,
+        @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Properties> {
+        console.log('Query: getAgentProperties');
+        return await this.propertyService.getAgentProperties(memberId, input);
+    }
 }
