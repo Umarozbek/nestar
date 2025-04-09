c7643826353d81d17f918a7e359fbbdb168ff7f0
UMARUZBEK <umarozbe@gmail.com>
2025-04-09 20:22:30 +0900

    feat:  create WEBSOCKET Connection process

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -8,6 +8,7 @@ import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
 import { T } from './libs/types/common';
+import { SocketModule } from './socket/socket.module';
 
 @Module({
   imports: [
@@ -29,7 +30,8 @@ import { T } from './libs/types/common';
       },
     }),
      ComponentsModule, //HTTP
-     DatabaseModule,// TCP
+     DatabaseModule,
+      SocketModule,// TCP
     //SOCKET => TCP  
     ],  
   controllers: [AppController], //REST API
--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 import { graphqlUploadExpress } from 'graphql-upload';
 import * as express from 'express';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 // express va nest jsni umumiy birlashmasi 
  
@@ -16,6 +17,7 @@ async function bootstrap() {
   app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10}));
   app.use('/uploads',express.static('./uploads'));
 
+  app.useWebSocketAdapter(new WsAdapter(app));
   await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
